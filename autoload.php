1bf71c40f6815d27452612177c3f38013331ebee
James <jakubtrtik2@gmail.com>
2019-09-02 01:04:39 +0200

    autoload: universal include path
    autoload: universal include path
    
    
    autoload: universal include path

--- autoload.php
+++ autoload.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
-set_include_path(get_include_path() . PATH_SEPARATOR . 'C:\root\webnodeapp');
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
 spl_autoload_extensions('.php');
 spl_autoload_register('myAutoloader');
 
 function myAutoloader($className)
 {
-	include 'C:\root\webnodeapp\\' . $className . '.php';
+	include __DIR__ . '\\' . $className . '.php';
 }
